24a8668b3d518d4bc2cc445e642cbeb16d011ff3
monam2 <kangcw0107@gmail.com>
2024-12-31 01:44:02 +0900

    #5 [Feat] font 인스턴스 파일 생성

---
+++ src/styles/fonts.ts
@@ -0,0 +1,6 @@
+import localFont from "next/font/local";
+
+export const binancePlex = localFont({
+  src: "../../public/fonts/BinancePLEX.ttf",
+  variable: "--binance-plex", // tailwind 변수로 사용
+});
