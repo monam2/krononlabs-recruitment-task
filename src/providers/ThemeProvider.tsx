314e88ac70ac91b3fb3bcd7fa32631f26a9b7a20
monam2 <kangcw0107@gmail.com>
2025-01-01 22:10:16 +0900

    #9 [Feat] 테마 컨텍스트를 관리하는 프로바이더 및 훅 생성

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,13 +1,64 @@
 "use client";
 
-import { cookies } from "next/headers";
-import { ReactNode, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 type LightTheme = "light" | "dark";
 type ChartTheme = "fresh" | "traditinal" | "colorVisionDeficiency";
 
-const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+interface ThemeProviderProps {
+  initialLightTheme: LightTheme;
+  initialChartTheme: ChartTheme;
+}
+
+interface ThemeContextValue extends ThemeProviderProps {
+  toggleLightTheme: () => void;
+  changeChartTheme: (theme: ChartTheme) => void;
+}
+
+const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
+
+const ThemeProvider = ({
+  initialLightTheme,
+  initialChartTheme,
+  children,
+}: React.PropsWithChildren<ThemeProviderProps>) => {
+  const [lightTheme, setLightTheme] = useState<LightTheme>(initialLightTheme);
+  const [chartTheme, setChartTheme] = useState<ChartTheme>(initialChartTheme);
+
+  useEffect(() => {
+    document.cookie = `lightTheme=${lightTheme}; max-age=604800`;
+    document.cookie = `chartTheme=${chartTheme}; max-age=604800`;
+  }, [lightTheme, chartTheme]);
+
+  const toggleLightTheme = () => {
+    setLightTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+  const changeChartTheme = (theme: ChartTheme) => {
+    setChartTheme(theme);
+  };
+
+  const value: ThemeContextValue = {
+    initialLightTheme,
+    initialChartTheme,
+    toggleLightTheme,
+    changeChartTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
+
+/** 다른 컴포넌트에서 테마 관련 기능을 사용하기 위한 커스텀 훅 */
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  return context;
+};
