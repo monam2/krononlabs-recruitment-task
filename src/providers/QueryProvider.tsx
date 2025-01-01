c898bbbc771ba70eba973cda3ce6bc1d99f8b43a
monam2 <kangcw0107@gmail.com>
2025-01-01 20:13:47 +0900

    #7 [Feat] 리액트 쿼리 프로바이더 추가

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      retry: 1,
+    },
+  },
+});
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
